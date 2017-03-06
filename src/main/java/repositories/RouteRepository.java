86d5541328e00d5b864e2a4fa2e021516bf79deb
JuanRa Rodríguez <dragondr3w@gmail.com>
2017-03-06 19:37:08 +0100

    Añadidas primeras Queries para el motor de búsqueda

--- src/main/java/repositories/RouteRepository.java
+++ src/main/java/repositories/RouteRepository.java
@@ -1,11 +1,23 @@
 package repositories;
 
+import java.sql.Date;
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Route;
 
 @Repository
 public interface RouteRepository extends JpaRepository<Route, Integer> {
-
+	
+	@Query("select r from Route r where r.origin LIKE %?2% AND r.destination LIKE %?3%")
+	Collection<Route> searchRoute(String origin, String destination);
+	
+	@Query("select r from Route r where r.date = ?1 AND r.origin LIKE %?2% AND r.destination LIKE %?3%")
+	Collection<Route> searchRouteAtDate(Date date, String origin, String destination);
+	
+	@Query("select r from Route r where r.departureTime >= ?1 AND r.origin LIKE %?2% AND r.destination LIKE %?3%")
+	Collection<Route> searchRouteAtTime(Date time, String origin, String destination);
 }
--- src/main/java/repositories/ShipmentRepository.java
+++ src/main/java/repositories/ShipmentRepository.java
@@ -1,6 +1,10 @@
 package repositories;
 
+import java.sql.Date;
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Shipment;
@@ -8,4 +12,12 @@ import domain.Shipment;
 @Repository
 public interface ShipmentRepository extends JpaRepository<Shipment, Integer> {
 
+	@Query("select s from Shipment s where s.origin LIKE %?2% AND s.destination LIKE %?3%")
+	Collection<Shipment> searchShipment(String origin, String destination);
+	
+	@Query("select s from Shipment s where s.date = ?1 AND s.origin LIKE %?2% AND s.destination LIKE %?3%")
+	Collection<Shipment> searchShipmentAtDate(Date date, String origin, String destination);
+	
+	@Query("select s from Shipment s where s.departureTime >= ?1 AND s.origin LIKE %?2% AND s.destination LIKE %?3%")
+	Collection<Shipment> searchShipmentAtTime(Date time, String origin, String destination);
 }
