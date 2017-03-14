923de49215f7dddf9703cd7fecd73e69444791e3
juanrarodriguez18 <dragondr3w@gmail.com>
2017-03-14 16:26:51 +0100

    Cambiado significado de la propiedad date

--- src/main/java/repositories/RouteRepository.java
+++ src/main/java/repositories/RouteRepository.java
@@ -12,7 +12,7 @@ import domain.Route;
 @Repository
 public interface RouteRepository extends JpaRepository<Route, Integer> {
 	
-	@Query("select r from Route r where r.origin LIKE %?1% AND r.destination LIKE %?2% AND (?3 IS NULL OR (year(r.date) = year(?3) AND month(r.date) = month(?3) AND day(r.date) = day(?3)))"
+	@Query("select r from Route r where r.origin LIKE %?1% AND r.destination LIKE %?2% AND (?3 IS NULL OR (year(r.departureTime) = year(?3) AND month(r.departureTime) = month(?3) AND day(r.departureTime) = day(?3)))"
 			+ "AND (?4 IS NULL OR r.departureTime >= ?4) AND (?5 IS NULL OR r.itemEnvelope = ?5 OR r.itemEnvelope LIKE 'both')"
 			+ "AND (?6 IS NULL OR (select sp.size from SizePrice sp where sp.route.id = r.id AND sp.size LIKE ?6) IS NOT NULL)")
 	Collection<Route> searchRoute(String origin, String destination, Date date, Date time, String envelope, String itemSize);
