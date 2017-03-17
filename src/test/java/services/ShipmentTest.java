f3f6984d53cb1f64dfce82f4c68d8454f35e5f5d
JuanRa Rodríguez <dragondr3w@gmail.com>
2017-03-17 09:58:35 +0100

    Eliminado SampleTest y terminado los tests unitarios de seleccionar envío

--- src/test/java/services/ShipmentTest.java
+++ src/test/java/services/ShipmentTest.java
@@ -12,31 +12,29 @@ import org.springframework.util.Assert;
 
 import domain.Shipment;
 import domain.ShipmentOffer;
+import domain.User;
 import utilities.AbstractTest;
 import utilities.UtilTest;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {
-		"classpath:spring/datasource.xml",
-		"classpath:spring/config/packages.xml"
-})
+@ContextConfiguration(locations = { "classpath:spring/datasource.xml", "classpath:spring/config/packages.xml" })
 @Transactional
 @TransactionConfiguration(defaultRollback = true)
-public class ShipmentTest extends AbstractTest{
-	
+public class ShipmentTest extends AbstractTest {
+
 	// Service to test --------------------------------------------------------
-	
+
 	@Autowired
 	private ShipmentService shipmentService;
-	
-	
-	// Supporting services ----------------------------------------------------	
-	
-	/*@Autowired
-	private UserService userService;*/
-	
-	// Test cases -------------------------------------------------------------	
-	
+
+	// Supporting services ----------------------------------------------------
+
+
+	 @Autowired 
+	 private UserService userService;
+
+	// Test cases -------------------------------------------------------------
+
 	/**
 	 * @Test Carry a Shipment
 	 * @result The shipment is associated to the carrier
@@ -46,56 +44,102 @@ public class ShipmentTest extends AbstractTest{
 		authenticate("user1");
 		Shipment shipment;
 		ShipmentOffer shipmentOffer;
-		
+
 		shipment = shipmentService.findOne(UtilTest.getIdFromBeanName("shipment1"));
-		Assert.isTrue(shipment.getCarried()==null);
-		
+		Assert.isTrue(shipment.getCarried() == null);
+
 		shipmentOffer = shipmentService.carryShipment(UtilTest.getIdFromBeanName("shipment1"));
-		Assert.isTrue(shipmentOffer.getId()!=0);
+		Assert.isTrue(shipmentOffer.getId() != 0);
 		Assert.isTrue(shipmentOffer.getShipment().equals(shipment));
-		
+
 		unauthenticate();
 	}
 
 	/**
-	 * @Test Other business methods
-	 * @result The methods returns the expected values
+	 * @Test Carry a Shipment
+	 * @result We create various offers from different users
 	 */
 	@Test
-	public void possitiveCommentOtherMethodsTest() {
+	public void possitiveCarryShipmentTest1() {
 		authenticate("user1");
+		Shipment shipment;
+		ShipmentOffer shipmentOffer;
+		ShipmentOffer shipmetOffer1;
+
+		shipment = shipmentService.findOne(UtilTest.getIdFromBeanName("shipment1"));
+		Assert.isTrue(shipment.getCarried() == null);
+
+		shipmentOffer = shipmentService.carryShipment(UtilTest.getIdFromBeanName("shipment1"));
+		Assert.isTrue(shipmentOffer.getId() != 0);
+		Assert.isTrue(shipmentOffer.getShipment().equals(shipment));
+
+		shipment = shipmentService.findOne(UtilTest.getIdFromBeanName("shipment1"));
+		Assert.isTrue(shipment.getCarried() == null);
+
+		shipmetOffer1 = shipmentService.carryShipment(UtilTest.getIdFromBeanName("shipment1"));
+		Assert.isTrue(shipmetOffer1.getId() != 0);
+		Assert.isTrue(shipmetOffer1.getShipment().equals(shipment));
 
 		unauthenticate();
 	}
 
 	/**
-	 * @Test Edition of comments
-	 * @result We try to edit an comment so
+	 * @Test Carry a Shipment
+	 * @result We try to carry our own shipment
 	 *         <code>IllegalArgumentException</code> is expected
 	 */
 	@Test(expected = IllegalArgumentException.class)
-	public void negativeCRUDTes() {		
-		throw new IllegalArgumentException();
+	public void negativeCarryShipmentTest1() {
+		authenticate("user2");
+		Shipment shipment;
+		ShipmentOffer shipmentOffer;
+
+		shipment = shipmentService.findOne(UtilTest.getIdFromBeanName("shipment1"));
+		Assert.isTrue(shipment.getCarried() == null);
+
+		shipmentOffer = shipmentService.carryShipment(UtilTest.getIdFromBeanName("shipment1"));
+		Assert.isTrue(shipmentOffer.getId() != 0);
+		Assert.isTrue(shipmentOffer.getShipment().equals(shipment));
+
+		unauthenticate();
 	}
 
 	/**
-	 * @Test Comments created by User
-	 * @result We try to get the comments of null user so
+	 * @Test Carry a Shipment
+	 * @result We try to carry a Shipment that doesn't exists
 	 *         <code>IllegalArgumentException</code> is expected
 	 */
 	@Test(expected = IllegalArgumentException.class)
-	public void negativeCommentOtherMethodsTest() {
-		throw new IllegalArgumentException();
+	public void negativeCarryShipmentTest2() {
+		authenticate("user1");
+		ShipmentOffer shipmentOffer;
+
+		shipmentOffer = shipmentService.carryShipment(-200);
+		Assert.isTrue(shipmentOffer.getId() != 0);
+
+		unauthenticate();
 	}
 
 	/**
-	 * @Test Comments created by Given User
-	 * @result We try to get the comments of null user so
+	 * @Test Carry a Shipment
+	 * @result We try carry a Shipment that is already carried
 	 *         <code>IllegalArgumentException</code> is expected
 	 */
 	@Test(expected = IllegalArgumentException.class)
-	public void negativeCommentOtherMethodsTest1() {
-		throw new IllegalArgumentException();
+	public void negativeCarryShipmentTest3() {
+		authenticate("user1");
+		Shipment shipment;
+		User user;
+		ShipmentOffer shipmentOffer;
+		
+		shipment = shipmentService.findOne(UtilTest.getIdFromBeanName("shipment1"));
+		user = userService.findByPrincipal();
+		shipment.setCarried(user);
+		
+		shipmentOffer = shipmentService.carryShipment(shipment.getId());
+		Assert.isTrue(shipmentOffer.getId() != 0);
+
+		unauthenticate();
 	}
 
 }
