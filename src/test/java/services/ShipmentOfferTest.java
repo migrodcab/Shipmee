e69d1a62c2ecf3912b6a9a9f293586bde4b2aace
Manuel López Ruiz <manuellr.git@gmail.com>
2017-03-18 20:27:09 +0100

    Test rechazar contraoferta transportista (#184)

---
+++ src/test/java/services/ShipmentOfferTest.java
@@ -0,0 +1,141 @@
+package services;
+
+import javax.transaction.Transactional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.util.Assert;
+
+import domain.Shipment;
+import domain.ShipmentOffer;
+import utilities.AbstractTest;
+import utilities.UtilTest;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring/junit.xml"})
+@Transactional
+@TransactionConfiguration(defaultRollback = true)
+public class ShipmentOfferTest extends AbstractTest {
+
+	// Service to test --------------------------------------------------------
+
+	@Autowired
+	private ShipmentOfferService shipmentOfferService;
+
+	// Supporting services ----------------------------------------------------
+
+	@Autowired
+	private ShipmentService shipmentService;
+
+
+	// Test cases -------------------------------------------------------------
+
+	/**
+	 * @Test Accept a Shipment Offer
+	 * @result The user accepts a shipment offer
+	 */
+	@Test
+	public void shipmentOfferAcceptPositive1() {
+		authenticate("user2");
+		
+		Shipment shipment;
+		ShipmentOffer ro;
+		
+		shipment = shipmentService.findOne(UtilTest.getIdFromBeanName("shipment1"));
+		ro = shipmentOfferService.findOne(UtilTest.getIdFromBeanName("shipmentOffer4"));
+		
+		shipmentOfferService.accept(ro.getId());
+		
+		ro = shipmentOfferService.findOne(ro.getId());
+		
+		Assert.isTrue(ro.getAcceptedBySender() && !ro.getRejectedBySender());
+		
+		for(ShipmentOffer a:shipmentOfferService.findAllByShipmentId(shipment.getId())){
+			if(a.getId() != ro.getId())
+				Assert.isTrue(!a.getAcceptedBySender());
+		}
+	}
+	
+	/**
+	 * @Test Accept a Shipment Offer
+	 * @result The user tries to accept their own shipment offer
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void shipmentOfferAcceptNegative1() {
+		authenticate("user2");
+		
+		ShipmentOffer ro;
+		
+		ro = shipmentOfferService.findOne(UtilTest.getIdFromBeanName("shipmentOffer3"));
+		
+		authenticate("user1");
+		
+		shipmentOfferService.accept(ro.getId());
+		
+		ro = shipmentOfferService.findOne(ro.getId());
+		
+		Assert.isTrue(ro.getAcceptedBySender() && !ro.getRejectedBySender());
+		
+	}
+	
+	/**
+	 * @Test Accept a Shipment Offer
+	 * @result The user tries to accept an offer without being logged in
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void shipmentOfferAcceptNegative2() {
+		authenticate("user2");
+		
+		ShipmentOffer ro;
+		
+		ro = shipmentOfferService.findOne(UtilTest.getIdFromBeanName("shipmentOffer3"));
+		
+		unauthenticate();
+		
+		shipmentOfferService.accept(ro.getId());
+		
+		ro = shipmentOfferService.findOne(ro.getId());
+		
+		Assert.isTrue(ro.getAcceptedBySender() && !ro.getRejectedBySender());
+	}
+	
+	/**
+	 * @Test Accept a Shipment Offer
+	 * @result The user tries to accept an offer that is rejected
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void shipmentOfferAcceptNegative3() {
+		authenticate("user2");
+		
+		ShipmentOffer ro;
+		
+		ro = shipmentOfferService.findOne(UtilTest.getIdFromBeanName("shipmentOffer2"));			
+		shipmentOfferService.accept(ro.getId());
+		ro = shipmentOfferService.findOne(ro.getId());
+		
+		Assert.isTrue(ro.getAcceptedBySender() && !ro.getRejectedBySender());
+	}
+	
+	/**
+	 * @Test Accept a Shipment Offer
+	 * @result The user tries to accept an offer that contain a shipmentOffer accepted
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void shipmentOfferAcceptNegative4() {
+		authenticate("user2");
+		
+		ShipmentOffer ro;
+		
+		ro = shipmentOfferService.findOne(UtilTest.getIdFromBeanName("shipmentOffer6"));				
+		shipmentOfferService.accept(ro.getId());
+		ro = shipmentOfferService.findOne(ro.getId());
+		
+		Assert.isTrue(ro.getAcceptedBySender() && !ro.getRejectedBySender());
+	}
+	
+
+}
